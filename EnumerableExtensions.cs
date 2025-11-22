c75a1eb2c7e3e2d5effe30b531dc404153572573
Liam Scholte <liam.a.scholte@gmail.com>
2025-11-22 13:55:51 -0800

    refactor: migrate extension from Powershell to C#

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDRManager
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable ?? Enumerable.Empty<T>();
+        }
+    }
+}
