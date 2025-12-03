46c83dbc987023588cdc1ecb502295c4141ff935
Liam Scholte <liam.a.scholte@gmail.com>
2025-12-02 18:44:54 -0800

    test: add unit tests for EmptyIfNull extension method

---
+++ test/HdrManager.Test/EnumerableExtensionsTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HdrManager.Test
+{
+    [TestFixture]
+    public class EnumerableExtensionsTest
+    {
+        [Test]
+        public void EmptyIfNull_ReturnsEmptyEnumerable_WhenInputIsNull()
+        {
+            IEnumerable<int> input = null;
+
+            var result = input.EmptyIfNull();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void EmptyIfNull_ReturnsSameEnumerable_WhenInputIsNotEmpty()
+        {
+            IEnumerable<int> input = new List<int>() { 1, 2, 3 };
+
+            var result = input.EmptyIfNull();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.SameAs(input));
+        }
+
+        [Test]
+        public void EmptyIfNull_ReturnsSameEnumerable_WhenInputIsEmpty()
+        {
+            IEnumerable<int> input = new List<int>();
+
+            var result = input.EmptyIfNull();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.SameAs(input));
+        }
+    }
+}
